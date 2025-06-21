26772ed4d6e02d11fa55bde1489fce35a5b87fdd
Juan Amaya <amayajuan95@gmail.com>
2025-06-21 12:41:31 -0500

    debug send email

--- src/controllers/register.controller.ts
+++ src/controllers/register.controller.ts
@@ -262,13 +262,18 @@ export class RegisterController {
             }
 
             for (const parent of parents) {
-                const sendEmail = await RegisterController.mailer.send({
-                    email: parent.email,
-                    subject: 'KINGDOM KIDS 2025 - REGISTRO EXITOSO',
-                    template: 'activation',
-                    kid: `${kidData.name} ${kidData.lastname}`,
-                    qrCode: imageUpload.image,
-                });
+                try {
+                    const sendEmail = await RegisterController.mailer.send({
+                        email: parent.email,
+                        subject: 'KINGDOM KIDS 2025 - REGISTRO EXITOSO',
+                        template: 'activation',
+                        kid: `${kidData.name} ${kidData.lastname}`,
+                        qrCode: imageUpload.image,
+                    });
+                    console.log('Resultado envío de correo:', sendEmail);
+                } catch (err) {
+                    console.log('Error enviando correo a:', parent.email, err);
+                }
             }
 
             return res.status(JsonResponse.OK).json({
